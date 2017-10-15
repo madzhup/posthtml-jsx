49a96f4f0038022eff6762255d6d521e87f111be
madzhup <madzhup@gmail.com>
2017-10-15 23:34:02 +0300

    add template option

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const jsx = require('./lib/jsx');
 const defaultRender = require('posthtml-render');
 
+const defaultTemplate = (name, props, content) =>
+`import React from 'react';
+
+const ${name} = (${props}) => (
+${content});
+
+export default ${name};
+`;
+
 module.exports = (options) => {
   options = options || {};
   options.props = options.props || '...props';
   options.render = options.render || defaultRender;
+  options.template = options.template || defaultTemplate;
 
   return function postHTMLJSX (tree) {
     return jsx(tree, options);
